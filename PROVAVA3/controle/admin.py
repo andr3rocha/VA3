9eaa11de7d9831d72379af216cd815ceb8704ab3
andr3rocha <andregire@hotmail.com>
2014-11-25 20:19:34 -0200

    Arrumar admin

--- PROVAVA3/controle/admin.py
+++ PROVAVA3/controle/admin.py
@@ -1,6 +1,6 @@
 #coding:utf-8
 from django.contrib import admin
-from models import Pessoa, lugar, validacao
+from models import Pessoa,lugar,validacao
 
 # Register your models here.
 
@@ -11,10 +11,15 @@ class PessoaAdmin(admin.ModelAdmin):
 	search_fields = ['Nome','CPF']
 
 class lugarAdmin(admin.ModelAdmin):
-	list_display = ['NomeLugas','nivelacesso']
+	list_display = ['NomeLugar','nivelacesso']
 	list_filter = ['NomeLugar']
 	search_fields = ['NomeLugar','nivelacesso']
 
+class validacaoAdmin(admin.ModelAdmin):
+	list_display = ['lugar','Pessoa','Entrada','Saida']
+	list_filter = ['lugar','Pessoa','Entrada','Saida']
+	search_fields = ['lugar','Pessoa','Entrada','Saida']
+
 admin.site.register(Pessoa,PessoaAdmin)
-admin.site.register(lugar)
-admin.site.register(validacao)
+admin.site.register(lugar,lugarAdmin)
+admin.site.register(validacao,validacaoAdmin)
